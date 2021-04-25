b8f85f811f322b85d5ad959950424bd744622529
Nehal <nehal.jn@gmail.com>
2021-04-25 11:06:25 +0530

    First Commit

---
+++ Maersk.Sorting.Api/IWorker.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Maersk.Sorting.Api
+{
+    public interface IWorker
+    {
+        public List<SortJob> ProcessSortJob(List<SortJob> job);
+    }
+}
